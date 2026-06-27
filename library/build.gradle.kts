970162f45030311d546ed8926e162b08580750e7
kuku! <am.abdulmueed5@gmail.com>
2026-06-27 14:25:12 +0500

    Migrate crypto from javax to cryptography-kotlin in library (#2813)

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -69,6 +69,7 @@ kotlin {
             implementation(libs.jsoup) // HTML Parser
             implementation(libs.rhino) // Run JavaScript
             implementation(libs.tmdb.java) // TMDB API v3 Wrapper Made with RetroFit
+            implementation(libs.bundles.cryptography) // Cryptography
 
             // Deprecated; will be removed once extensions have time to migrate from using it
             implementation("me.xdrop:fuzzywuzzy:1.4.0")
