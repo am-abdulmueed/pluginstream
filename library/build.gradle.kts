9b781f79d7ae4b01f56b375b7c4b5936d907577f
Your Name <am.abdulmueed5@gmail.com>
2026-07-13 22:33:22 +0500

    [skip ci] Use abiValidation to prevent breaking changes in extension library (#2835)

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -90,6 +90,19 @@ kotlin {
     }
 }
 
+
+    @OptIn(org.jetbrains.kotlin.gradle.dsl.abi.ExperimentalAbiValidation::class)
+    // https://kotlinlang.org/docs/gradle-binary-compatibility-validation.html
+    abiValidation {
+        enabled.set(true)
+        this.filters {
+            exclude {
+                annotatedWith.add("com.lagradost.cloudstream3.Prerelease")
+                annotatedWith.add("com.lagradost.cloudstream3.InternalAPI")
+            }
+        }
+    }
+    
 tasks.withType<KotlinJvmCompile> {
     compilerOptions {
         jvmTarget.set(javaTarget)
