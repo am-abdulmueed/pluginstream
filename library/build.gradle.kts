53d75d7fcee9f82bbea08c0a157679b7c395d6c4
Your Name <am.abdulmueed5@gmail.com>
2026-07-13 22:05:48 +0500

    Remove fuzzywuzzy from library (#2999)

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -71,8 +71,6 @@ kotlin {
             implementation(libs.tmdb.java) // TMDB API v3 Wrapper Made with RetroFit
             implementation(libs.bundles.cryptography) // Cryptography
 
-            // Deprecated; will be removed once extensions have time to migrate from using it
-            implementation("me.xdrop:fuzzywuzzy:1.4.0")
         }
 
         commonTest.dependencies {
